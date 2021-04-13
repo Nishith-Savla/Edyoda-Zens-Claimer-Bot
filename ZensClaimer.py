511d30633065d7b95262d4cbba058d9fe7fb257c
Nishith-Savla <nishithsavla005@gmail.com>
2021-04-13 22:00:11 +0530

    Change max line length to 79 chars

--- ZensClaimer.py
+++ ZensClaimer.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 
-from conf import CHROMEDRIVER_PATH, USERNAME, PASSWORD
+from conf import CHROMEDRIVER_PATH, PASSWORD, USERNAME
 
 
 def connect(executable_path=CHROMEDRIVER_PATH) -> webdriver.Chrome:
@@ -18,6 +18,7 @@ def connect(executable_path=CHROMEDRIVER_PATH) -> webdriver.Chrome:
     ----------
     executable_path: str
         The path to the chromedriver executable.
+
     Returns
     -------
     driver: webdriver.Chrome
@@ -40,21 +41,30 @@ def login(driver: webdriver.Chrome, username=USERNAME, password=PASSWORD):
     """
     driver.get("https://edyoda.com")  # Open Edyoda website
     wait: WebDriverWait = WebDriverWait(driver, 15)
-    wait.until(ec.invisibility_of_element((By.XPATH, '//div[contains(@class, "Loader")]/svg')))
+    wait.until(ec.invisibility_of_element(
+        (By.XPATH, '//div[contains(@class, "Loader")]/svg')))
     wait.until(
-        ec.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Log") or contains(@id, "login")]'))).click()
+        ec.element_to_be_clickable(
+            (By.XPATH,
+             '//button[contains(text(), "Log") or contains(@id, "login")]'))
+    ).click()
     wait.until(ec.presence_of_element_located(  # Type username
-        (By.XPATH, '//div[contains(@class, "Input")]/label[contains(text(), "sername")]/following-sibling::input')
+        (By.XPATH,
+         '//div[contains(@class, "Input")]/label[contains(text(), "sername")]/'
+         'following-sibling::input')
     )).send_keys(username)
     driver.find_element_by_xpath(  # Type password
-        '//div[contains(@class, "Input")]/label[contains(text(), "assword")]/''following-sibling::input'
+        '//div[contains(@class, "Input")]/label[contains(text(), "assword")]/'
+        'following-sibling::input'
     ).send_keys(password)
     driver.find_element_by_xpath(  # Sign In
-        '//button[contains(text(), "SIGN IN") and contains(@class, "NonMobile")]').click()
+        '//button[contains(text(), "SIGN IN") and '
+        'contains(@class, "NonMobile")]').click()
 
 
 def claim(driver: webdriver.Chrome) -> bool:
-    """Tries to claim the daily Zens and returns True if successfully claimed, else False.
+    """Tries to claim the daily Zens and returns True if successfully claimed,
+     else False.
 
     Parameters
     ----------
@@ -69,15 +79,20 @@ def claim(driver: webdriver.Chrome) -> bool:
     time.sleep(3)
     try:
         driver.find_element_by_xpath(  # Click on daily zens icon
-            '//li[contains(@class, "Container_")]//img[contains(@alt, "EdYoda Daily")]').click()
+            '//li[contains(@class, "Container_")]//img[contains(@alt, '
+            '"EdYoda Daily")]').click()
         driver.find_element_by_xpath(  # Claim zens
-            '//li[contains(@class, "Container_")]//button[contains(text(), "CLAIM") and contains(@class, "NonMobile")]'
+            '//li[contains(@class, "Container_")]//button[contains(text(), '
+            '"CLAIM") and contains(@class, "NonMobile")]'
         ).click()
     except NoSuchElementException:
         return False
     try:
-        WebDriverWait(driver, 10).until(ec.presence_of_element_located(  # Click Done
-            (By.XPATH, '//button[contains(text(), "DONE") and contains(@class, "NonMobile")]'))).click()
+        WebDriverWait(driver, 10).until(
+            ec.presence_of_element_located(  # Click Done
+                (By.XPATH,
+                 '//button[contains(text(), "DONE") and '
+                 'contains(@class, "NonMobile")]'))).click()
     except TimeoutException:
         print("Done dialog not found")
         return False
